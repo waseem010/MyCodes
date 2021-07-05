171215e3c99d6cf58843544ec8d774b15a7888b6
waseem010 <71478157+waseem010@users.noreply.github.com>
2021-07-05 21:37:52 +0530

    Add files via upload

---
+++ CP/Array/STL/list.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+list<int> l;
+list<int> l2{1,2,3,45,4,3,33,3,4444};
+list<string> l3{"apple,mango,pineapple"};
+
+ 
+cout<<l.size()<<endl;
+cout<<l.size()<<endl;
+cout<<l.size()<<endl;
+
+
+return 0;
+}
